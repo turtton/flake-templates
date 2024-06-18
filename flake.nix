4424db6da3dae95013caa4084e5467868c819c61
turtton <top.gear7509@turtton.net>
2024-06-18 21:00:30 +0900

    Format flake file
    Signed-off-by: turtton <top.gear7509@turtton.net>

--- flake.nix
+++ flake.nix
@@ -4,21 +4,22 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-        devShells.default = pkgs.mkShell {
-          packages = [ pkgs.bashInteractive ];
-        };
-      }) // {
-        templates = {
-          corepack = {
-            path = ./templates/corepack;
-            description = "nix flake new -t github:turtton/flake-templates#corepack";
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
+          devShells.default = pkgs.mkShell {
+            packages = [ pkgs.bashInteractive ];
           };
+        }) // {
+      templates = {
+        corepack = {
+          path = ./templates/corepack;
+          description = "nix flake new -t github:turtton/flake-templates#corepack";
         };
-			};
+      };
+    };
 }
