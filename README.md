60912d91a780c3875a2e282d208d7d00c627715d
turtton <top.gear7509@turtton.net>
2024-06-18 20:59:34 +0900

    Add corepack template
    Signed-off-by: turtton <top.gear7509@turtton.net>

---
+++ README.md
@@ -0,0 +1,14 @@
+# flake-templates
+
+Usage:
+
+```sh
+nix flake init -t github:turtton/flake-templates#{name}
+```
+
+All templates based on [nix-direnv](https://github.com/nix-community/nix-direnv) flake template
+
+## Template list
+
+- `corepack`
+  Please craete `package.json` and specify `packageManager`(use: `corepack use pnpm`) before use this template.
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.bashInteractive ];
+        };
+      }) // {
+        templates = {
+          corepack = {
+            path = ./templates/corepack;
+            description = "nix flake new -t github:turtton/flake-templates#corepack";
+          };
+        };
+			};
+}
