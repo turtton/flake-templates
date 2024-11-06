5c5a948c885c1884d97eea8a1a4ce3825dba46da
turtton <top.gear7509@turtton.net>
2024-11-06 17:29:13 +0900

    Add crane

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,19 +1,123 @@
 {
+  #	Ref: https://github.com/ipetkov/crane/tree/master/examples/quick-start
   description = "Falke for Rust using direnv";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-utils, crane, fenix, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
       };
+      craneLib = crane.mkLib pkgs;
+      src = craneLib.cleanCargoSource ./src;
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+
+        buildInputs = [
+          # Add additional build inputs here
+        ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          # Additional darwin specific inputs can be set here
+          pkgs.libiconv
+        ];
+
+        # Additional environment variables can be set directly
+        # MY_CUSTOM_VAR = "some value";
+      };
+
+      craneLibLLvmTools = craneLib.overrideToolchain
+        (fenix.packages.${system}.complete.withComponents [
+          "cargo"
+          "llvm-tools"
+          "rustc"
+        ]);
+
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      # Build the actual crate itself, reusing the dependency
+      # artifacts from above.
+      my-crate = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+      });
     in
     with pkgs; {
+      checks = {
+        # Build the crate as part of `nix flake check` for convenience
+        inherit my-crate;
+
+        # Run clippy (and deny all warnings) on the crate source,
+        # again, reusing the dependency artifacts from above.
+        #
+        # Note that this is done as a separate derivation so that
+        # we can block the CI if there are issues here, but not
+        # prevent downstream consumers from building our crate by itself.
+        my-crate-clippy = craneLib.cargoClippy (commonArgs // {
+          inherit cargoArtifacts;
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+        });
+
+        my-crate-doc = craneLib.cargoDoc (commonArgs // {
+          inherit cargoArtifacts;
+        });
+
+        # Check formatting
+        my-crate-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+
+        # my-crate-toml-fmt = craneLib.taploFmt {
+        #   src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
+        #   # taplo arguments can be further customized below as needed
+        #   # taploExtraArgs = "--config ./taplo.toml";
+        # };
+
+        # Audit dependencies
+        my-crate-audit = craneLib.cargoAudit {
+          inherit src advisory-db;
+        };
+
+        # Audit licenses
+        my-crate-deny = craneLib.cargoDeny {
+          inherit src;
+        };
+
+        # Run tests with cargo-nextest
+        # Consider setting `doCheck = false` on `my-crate` if you do not want
+        # the tests to run twice
+        my-crate-nextest = craneLib.cargoNextest (commonArgs // {
+          inherit cargoArtifacts;
+          partitions = 1;
+          partitionType = "count";
+        });
+      };
+
+      packages = {
+        default = my-crate;
+      } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+        my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      };
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = my-crate;
+      };
+
       formatter = nixpkgs-fmt;
+
       devShells.default = mkShell {
         nativeBuildInputs = [ pkg-config ];
         # buildInputs = [ openssl ];
