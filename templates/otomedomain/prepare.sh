83ed7c8a580457c1e1fe3b72b2f92ed6cd9e4fdf
turtton <top.gear7509@turtton.net>
2024-12-01 22:35:10 +0900

    Fix script

--- templates/otomedomain/prepare.sh
+++ templates/otomedomain/prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 wineprefix cjkfonts
-cd "./オトメ＊ドメイン/オトメドメインＤＬ版・期間限定コラボセット/オトメドメイン/"
+cd "./オトメドメインＤＬ版・期間限定コラボセット/オトメドメイン/"
 mv オトメドメイン.exe オトメドメイン-old.exe
 dd bs=1 skip=229376 if=オトメドメイン-old.exe of=オトメドメイン.exe
